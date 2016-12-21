caf606eaa44ee9b31f2a41917e05cbfd2a896022
Elmar Ritsch <elritsch@users.noreply.github.com>
2016-12-21 14:47:08 +0100

    Initial Commit

---
+++ pickr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pickr
+//
+//  Created by Elmar Ritsch on 21.12.16.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
